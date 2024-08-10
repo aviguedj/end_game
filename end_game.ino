2b1215fa33169db4107bca8154ed24a3905938a1
aviguedj <aviguedj555@gmail.com>
2024-08-10 21:58:25 +0300

    בדיקת הכפתור והוספת משתנה
    הוספת משתנה, בדיקת הכפתור ועדכון מצב הכפתור

--- end_game.ino
+++ end_game.ino
@@ -12,7 +12,7 @@
 
 #define COUNTER 4
 
-#define DEBOUNCE_DELAY 50 
+#define DEBOUNCE_DELAY 50
 
 const int buttonPins[COUNTER] = { RBtn, YBtn, GBtn, BBtn };
 const int ledPins[COUNTER] = { RLed, YLed, GLed, BLed };
@@ -21,19 +21,18 @@ const int tones[COUNTER] = { 1000, 1500, 2000, 2500 };
 void setup(){
   pinMode( BZR , OUTPUT);
   randomSeed(analogRead(A1));
+  
 
 }
 
 void loop(){
-static unsigned long lastPressTimes[COUNTER] = {0}; 
-static unsigned long pressDurations[COUNTER] = {0};
+static unsigned long lastPressTimes[COUNTER] = {0};
+  static unsigned long pressDurations[COUNTER] = {0};
 
   for (int i = 0; i < COUNTER; i++) {
     if (checkButtonPress(buttonPins[i], lastPressTimes[i], pressDurations[i])) {
-      
       digitalWrite(ledPins[i], HIGH);
     } else {
-      
       digitalWrite(ledPins[i], LOW);
     }
   }
@@ -44,22 +43,24 @@ static unsigned long pressDurations[COUNTER] = {0};
 
 
 
-bool checkButtonPress(int buttonPin, unsigned long &lastPressTime, unsigned long &pressDuration) {
-  
-  bool buttonState = digitalRead(buttonPin);
 
-  if (buttonState == HIGH) {
+bool checkButtonPress(int buttonPin, unsigned long &lastPressTime, unsigned long &pressDuration) {
+    static bool lastButtonState = LOW;
+    bool buttonState = digitalRead(buttonPin);
+
     unsigned long currentTime = millis();
-    if (currentTime - lastPressTime > DEBOUNCE_DELAY) {
-      pressDuration = currentTime - lastPressTime;
-      lastPressTime = currentTime;
-      return true; 
+    if (buttonState != lastButtonState) {
+        lastPressTime = currentTime;
     }
-  } else {
-    
-    lastPressTime = millis();
-    pressDuration = 0;
-  }
-  return false; 
+    if (buttonState == HIGH && (currentTime - lastPressTime) > DEBOUNCE_DELAY) {
+        pressDuration = currentTime - lastPressTime;
+        lastPressTime = currentTime;
+        lastButtonState = buttonState; 
+        return true;
+    } else if (buttonState == LOW) {
+        pressDuration = 0;
+        lastButtonState = buttonState; 
+    }
+    return false;
 }
 
